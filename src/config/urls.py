343e8b07715de594390a3a8dc06fc7cb19e596f3
Saqib <soomrosaqib32@gmail.com>
2025-10-16 13:00:33 +0500

    made small changes in setting.py, some template chanages removed buttons on hero section, added robots.txt

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,12 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from irab import views, api_views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('irab.urls')), 
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain"
+            ),
+        ),
 ]
 
 
